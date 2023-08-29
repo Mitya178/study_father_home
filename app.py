c3b55bc3810b3824a2d294298c09ffa6484e1324
Dmitriy Voronkov <58822703+Mitya178@users.noreply.github.com>
2023-08-29 10:27:00 +0300

    port 9090

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7070, debug=True)
+    app.run(host='0.0.0.0', port=9090, debug=True)
